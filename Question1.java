78db81a344cad778a04a56821ed68f9d0e189365
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-04 19:35:01 +0000

    Addedd comment to Question 1

--- Question1.java
+++ Question1.java
@@ -1,8 +1,16 @@
 public class Question1 {
+    //This is my main method
     public static void main(String[] args) {
-        executeGradeReport(20);
+        executeGradeReport(1000);
         
     }
+
+    /*  1. I initiated the executeGradeReport method here
+        2. It does not perform any calculations but instead it calls the following methods;
+            a. validateScore(score) - Validates score
+            b. calculateLetterGrade - It assigns a letter a grade to the score
+            c. displayPerformanceMessage - It displays a message of students performance based on their grade.
+    */ 
     public static void executeGradeReport(double score) {
         if(!validateScore(score)) {
             System.out.println("Invalid score");
@@ -14,6 +22,11 @@ public class Question1 {
         
 
     }
+
+    /*
+        validateScore.java - Checks if score ranges from 0 to 100
+        if it is valid, returns true else, it returns false
+    */
     public static boolean validateScore(double score) {
         boolean valid = true;
         if (score >= 0 && score <= 100) {
@@ -24,6 +37,10 @@ public class Question1 {
         return valid;
 
     }
+    /*
+        if validate score returns true, it assigns a letter grade, A,B,C,D and F.
+    */
+
     public static char calculateLetterGrade(double score) {
         char assignGrade;
         if (score >= 80) {
@@ -41,6 +58,10 @@ public class Question1 {
         return assignGrade;
     }
 
+
+    /*
+        Checks the letter grade and displays a performance message based on letter grade.
+    */
     public static void displayPerformanceMessage(char studentGrade) {
         if (studentGrade == 'A') {
             System.out.println("You perform exceptionally well.!!");
